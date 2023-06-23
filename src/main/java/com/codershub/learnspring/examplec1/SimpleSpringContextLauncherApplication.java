d4ee4584e49179b98ebdcb7eb0d9d0e3ea54280d
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-06-23 14:21:54 +0100

    lazy init

--- src/main/java/com/codershub/learnspring/examplec1/SimpleSpringContextLauncherApplication.java
+++ src/main/java/com/codershub/learnspring/examplec1/SimpleSpringContextLauncherApplication.java
@@ -3,19 +3,38 @@ package com.codershub.learnspring.examplec1;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
+@Component
+class ClassA{
+
+}
+
+@Component
+@Lazy
+class  ClassB{
+    private  ClassA classA;
+
+    public  ClassB(ClassA classA){
+        System.out.println("Some init Logic Goes here");
+        this.classA = classA;
+    }
+    public  void doSomething(){
+        System.out.println("Do something here");
+    }
+
+}
 @Configuration
  @ComponentScan
  public class SimpleSpringContextLauncherApplication {
     public static void main (String[] args) {
-        var context =
-                     new AnnotationConfigApplicationContext
-                             (SimpleSpringContextLauncherApplication.class);
+        var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class);
+        System.out.println("Inilization of Context is completed");
+        context.getBean(ClassB.class).doSomething();
 
-            Arrays.stream(context.getBeanDefinitionNames())
-                    .forEach(System.out::println);
-        System.out.println( context.getBean(BusinessCalculationService.class).findMax());
+    }
 
-        }
     }
