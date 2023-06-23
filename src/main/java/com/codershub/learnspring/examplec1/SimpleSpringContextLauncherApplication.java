a1c358f57b221e697375becdeb4961884a0f9a2f
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-06-23 16:12:46 +0100

    wrap up

--- src/main/java/com/codershub/learnspring/examplec1/SimpleSpringContextLauncherApplication.java
+++ src/main/java/com/codershub/learnspring/examplec1/SimpleSpringContextLauncherApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
