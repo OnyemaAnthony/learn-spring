d9616e3d18c0cfe887f6f1ff7917e3727c3f6186
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-06-23 15:25:01 +0100

    xml beam

--- src/main/java/com/codershub/learnspring/examplec1/SimpleSpringContextLauncherApplication.java
+++ src/main/java/com/codershub/learnspring/examplec1/SimpleSpringContextLauncherApplication.java
@@ -1,39 +1,22 @@
 package com.codershub.learnspring.examplec1;
-
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.Annotation;
 import java.util.Arrays;
-@Component
-class ClassA{
 
-}
-
-@Component
-@Lazy
-class  ClassB{
-    private  ClassA classA;
-
-    public  ClassB(ClassA classA){
-        System.out.println("Some init Logic Goes here");
-        this.classA = classA;
-    }
-    public  void doSomething(){
-        System.out.println("Do something here");
-    }
-
-}
-@Configuration
- @ComponentScan
- public class SimpleSpringContextLauncherApplication {
+public class SimpleSpringContextLauncherApplication {
     public static void main (String[] args) {
-        var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class);
-        System.out.println("Inilization of Context is completed");
-        context.getBean(ClassB.class).doSomething();
+        var context = new ClassPathXmlApplicationContext("config.xml");
+        Arrays.stream(context.getBeanDefinitionNames ()).forEach (System.out::println);
+        System.out.println(context.getBean("name"));
+
 
     }
 
