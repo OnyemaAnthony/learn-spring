7a274c309884519c891c63f6db11ac7dcbb850e9
Anthonychibuike <Anthonychibuike246@gmail.com>
2023-08-05 22:11:44 +0100

    update app0

--- src/main/java/com/codershub/learnspring/App02HelloWorldSpring.java
+++ src/main/java/com/codershub/learnspring/App02HelloWorldSpring.java
@@ -1,16 +1,11 @@
 package com.codershub.learnspring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
      var context =    new AnnotationConfigApplicationContext(HelloWorldConfig.class);
-//        System.out.println(context.getBean("age"));
-//        System.out.println(context.getBean("person"));
-//        System.out.println(context.getBean(Address.class  ));
         System.out.println(context.getBeanDefinitionNames());
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
